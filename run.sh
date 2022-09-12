b7cbe8eebd7454935f842da019a3ff6b0d3a0cc7
rashmi sah <105689930+rashmi-sah@users.noreply.github.com>
2022-09-12 23:39:10 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
+echo "\n\n\n\n\n############# CREATE BUCKET #####################\n\n\n\n\n"
 aws s3 mb s3://buck12sept22
+echo "\n\n\n\n\n############# LIST BUCKET #####################\n\n\n\n\n"
 aws s3 ls
+echo "\n\n\n\n\n############# SYNC BUCKET #####################\n\n\n\n\n"
 aws s3 sync . s3://buck12sept22
+echo "\n\n\n\n\n############# LIST BUCKET CONTENT #####################\n\n\n\n\n"
 aws s3 ls s3://buck12sept22
+echo "\n\n\n\n\n############# DELETE BUCKET #####################\n\n\n\n\n"
 aws s3 rb s3://buck12sept22 --force
